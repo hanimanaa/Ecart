f7c3954a1e70882baa9129a189e0deca4a9633fc
hani_pc <hanimanaa@gmail.com>
2019-08-24 01:16:59 +0300

    Initial commit

---
+++ app/src/main/java/com/dimatechs/ecart/HomeActivity.java
@@ -0,0 +1,148 @@
+package com.dimatechs.ecart;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.dimatechs.ecart.Model.Products;
+import com.dimatechs.ecart.ViewHolder.ProductViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import io.paperdb.Paper;
+
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener  {
+
+    private Button RegisterAccountBtn;
+    private FloatingActionButton btn;
+    private DatabaseReference ProductsRef;
+    private RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Paper.init(this);
+
+        btn = (FloatingActionButton) findViewById(R.id.btn);
+        btn.setOnClickListener(this);
+
+        ProductsRef= FirebaseDatabase.getInstance().getReference().child("Products");
+
+        recyclerView=findViewById(R.id.recycler_menu);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view==RegisterAccountBtn) {
+
+        }
+        else if (view==btn){
+            Toast.makeText(HomeActivity.this, "סל קניות", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(getApplicationContext(), CartActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        FirebaseRecyclerOptions<Products> options=
+                new FirebaseRecyclerOptions.Builder<Products>()
+                .setQuery(ProductsRef,Products.class)
+                .build();
+
+
+        FirebaseRecyclerAdapter<Products, ProductViewHolder> adapter=
+                new FirebaseRecyclerAdapter<Products, ProductViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull ProductViewHolder holder, int position, @NonNull final Products model)
+                    {
+                        holder.txtProductName.setText(model.getName());
+                        holder.txtProductDescription.setText(model.getDescription());
+                        holder.txtProductPrice.setText(" מחיר : " +model.getPrice() + " ש\"ח ");
+                        Picasso.get().load(model.getImage()).into(holder.imageView);
+
+
+                        holder.itemView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view)
+                            {
+                                Intent intent = new Intent(HomeActivity.this,ProductDetailsActivity.class);
+                                intent.putExtra("pid",model.getPid());
+                                startActivity(intent);
+
+                            }
+                        });
+                    }
+
+                    @Override
+                    public ProductViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+                    {
+                        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.product_items_layout,parent,false);
+                        ProductViewHolder holder = new ProductViewHolder(view);
+                        return holder;
+                    }
+                };
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+
+        if (id == R.id.action_Update) {
+            Intent intent = new Intent(HomeActivity.this, SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        else if (id == R.id.action_Exit) {
+            Toast.makeText(this,"יציאה",Toast.LENGTH_LONG).show();
+         //   android.os.Process.killProcess(android.os.Process.myPid());
+         // System.exit(1);
+         // Paper.book().destroy();
+
+        Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+            return true;
+        }
+        return true;
+    }
+}
+
+
